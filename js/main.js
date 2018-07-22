f83616b530b25a826cb1afdc5aeed77c772348c1
Harry Thuku <harsygeorgee@gmail.com>
2018-07-22 16:05:57 +0300

    adds a fully interactive js code that hides and shows buttons necesarily

--- js/main.js
+++ js/main.js
@@ -3,3 +3,33 @@
 // })
 
 
+$().ready(function(){
+
+    $("#btnP1Roll").click(function(event){
+        event.preventDefault();
+        $("#btnP2Roll").hide(200);
+        $("#btnP2Hold").hide(200);
+        $("#btnP1Hold").show(200);
+    })
+
+    $("#btnP1Hold").click(function(event){
+        event.preventDefault();
+        $("#btnP1Roll").hide(200);
+        $("#btnP2Roll").show(200);
+        $("#btnP1Hold").hide(200);
+    })
+
+    $("#btnP2Roll").click(function(event){
+        event.preventDefault();
+        $("#btnP1Roll").hide(200);
+        $("#btnP1Hold").hide(200);
+        $("#btnP2Hold").show(200);
+    })
+
+    $("#btnP2Hold").click(function(event){
+        event.preventDefault();
+        $("#btnP2Roll").hide(200);
+        $("#btnP1Roll").show(200);
+        $("#btnP2Hold").hide(200);
+    })
+})
