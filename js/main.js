a778bb68309e281a1e63c695268202b2cc1feba7
Harry Thuku <harsygeorgee@gmail.com>
2018-07-24 18:43:46 +0300

    adds files from remote

--- js/main.js
+++ js/main.js
@@ -20,11 +20,20 @@ function winner(){
     }
 };
 
+// roll dice
+function rollDice(){
+    return Math.floor((Math.random() * 6) + 1);
+};
+
 
 // Player1:
 var player1Name;
 var playerOneScore=0;
 var playerOneRolls=0;
+
+function p1rollDice(){
+    return playerOneScore=playerOneScore+rollDice();
+}
 
 function counter1(){
     if(playerOneRolls<=5){
@@ -71,6 +80,7 @@ function p1hold(){
     $("#btnP1Hold").hide(200);
     $("#comnConsole").text(">><<Game says: Dice passed to "+player2Name+". Your men are counting on you!");
     gameover();
+    $("#p1Score").text(playerOneScore);
     winner();
 };
 
@@ -78,6 +88,9 @@ function p1hold(){
 var player2Name;
 var playerTwoScore=0;
 var playerTwoRolls=0;
+function p2rollDice(){
+    return playerTwoScore=playerTwoScore+rollDice();
+}
 function counter2(){
     if(playerTwoRolls<=5){
         $("#btnP1Roll").hide(200);
@@ -123,6 +136,7 @@ function p2hold(){
         $("#btnP2Hold").hide(200);
         $("#comnConsole").text(">><<Game says: Dice passed to "+player1Name+". Roll carefully!");
         gameover();
+        $("#p2Score").text(playerTwoScore);
         winner();
 };
 
@@ -218,127 +232,3 @@ $().ready(function(){
         rematch();
     });
 });
-
-
-
-
-
-
-// var rollVal=3;
-
-
-
-// function rolledVal(){
-//     if(rollVal<=4){
-//         $("#diceValP1").text('<img src"../images/diceFace/26551.jpg">');
-//     }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.addEventListener('load', () => {
-	var
-		carousels = document.querySelectorAll('.carousel')
-	;
-
-	for (var i = 0; i < carousels.length; i++) {
-		carousel(carousels[i]);
-	}
-});
-
-function carousel(root) {
-	var
-		figure = root.querySelector('figure'),
-		nav = root.querySelector('nav'),
-		images = figure.children,
-		n = images.length,
-		gap = root.dataset.gap || 0,
-		bfc = 'bfc' in root.dataset,
-		
-		theta =  2 * Math.PI / n,
-		currImage = 0
-	;
-	
-	setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
-	window.addEventListener('resize', () => { 
-		setupCarousel(n, parseFloat(getComputedStyle(images[0]).width)) 
-	});
-
-	setupNavigation();
-
-	function setupCarousel(n, s) {
-		var	
-			apothem = s / (2 * Math.tan(Math.PI / n))
-		;
-		
-		figure.style.transformOrigin = `50% 50% ${- apothem}px`;
-
-		for (var i = 0; i < n; i++)
-			images[i].style.padding = `${gap}px`;
-		for (i = 1; i < n; i++) {
-			images[i].style.transformOrigin = `50% 50% ${- apothem}px`;
-			images[i].style.transform = `rotateY(${i * theta}rad)`;
-		}
-		if (bfc)
-			for (i = 0; i < n; i++)
-				 images[i].style.backfaceVisibility = 'hidden';
-		
-		rotateCarousel(currImage);
-	}
-
-	function setupNavigation() {
-		nav.addEventListener('click', onClick, true);
-		
-		function onClick(e) {
-			e.stopPropagation();
-			
-			var t = e.target;
-			if (t.tagName.toUpperCase() != 'BUTTON')
-				return;
-			
-			if (t.classList.contains('next')) {
-				currImage++;
-			}
-			else {
-				currImage--;
-			}
-			
-			rotateCarousel(currImage);
-		}
-			
-	}
-
-	function rotateCarousel(imageIndex) {
-		figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
-	}
-	
-}
